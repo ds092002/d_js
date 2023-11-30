b1e39d7f7e8119599f978dbd14ff777caa965703
Dhaval Solanki <dhavalas24@gmail.com>
2023-11-30 16:50:51 +0530

    30 Nov

---
+++ 3.js
@@ -0,0 +1,254 @@
+/* 1 = Find Smallest */
+// let a = Number(prompt("Enter a : "));
+// let b = Number(prompt("Enter b : "));
+
+// if (a > b) {
+//     console.log("Value of a is : "+a);
+//     console.log("A is Big");
+// }
+// else{
+//     console.log("Value of b is : "+b);
+//     console.log("A is Small");
+// }
+
+
+/* 2 = Positive Or Negitive*/
+
+// let a = Number(prompt("Enter a : "));
+// console.log("Enter A value : "+a);
+
+// if (0 < a) {
+//     console.log("A is Positive ");
+// }
+// else{
+//     console.log("A is Negative ");
+// }
+
+/* 3 = Divisible By 5*/
+
+// let n = Number(prompt("Enter n : "));
+// console.log("Enter Value : "+n);
+
+// if (n%5==0) {
+//     console.log("Its Divisible By 5");
+// }
+// else{
+//     console.log("Its Not Divisible By 5");
+// }
+
+/* 4 = Divisible By 5 Or 3*/
+
+// let n = Number(prompt("Enter n : "));
+// console.log("Enter Value : "+n);
+
+// if (n%5==0) {
+//     console.log("Its Divisible By 5 ");
+// }
+// else{
+//     console.log("Its Not Divisible By 5 ");
+// }
+// if (n%3==0) {
+//     console.log("Its Divisible By 3 ");
+// }
+// else{
+//     console.log("Its Not Divisible By 3 ");
+// }
+
+/* 5 = Students Marks */
+
+// let a = Number(prompt("Enter Maths : "));
+// let b = Number(prompt("Enter Science : "));
+// let c = Number(prompt("Enter English : "));
+
+// console.log("Maths Marks : "+a);
+// console.log("Science Marks : "+b);
+// console.log("English Marks : "+c);
+
+// let marks = a + b + c;
+// let per = marks / 3;
+
+// console.log("Obtain Marks :"+marks);
+// console.log("Perchantage : "+per);
+
+// if (marks < 33) {
+//     console.log("Student Pass");
+// }
+// else {
+//     console.log("Student Not Pass");
+// }
+
+/* 6 = leap year */
+
+
+// let a = Number(prompt("Enter a : "));
+// console.log("Enter  value : "+a);
+
+// if (a%4==0) {
+//     console.log("Leap Year");
+// }
+// else{
+//     console.log("Not Leap Year");
+// }
+
+/* 7 = a,b,c Largest Among */
+
+
+// let a = Number(prompt("Enter a : "));
+// let b = Number(prompt("Enter b : "));
+// let c = Number(prompt("Enter c : "));
+
+// console.log("Value of a is : "+a);
+// console.log("Value of b is : "+b);
+// console.log("Value of c is : "+c);
+
+// if (a > b && a > c) {
+//     console.log("A is Big");
+// }
+// else if(!(b < c)){
+//     console.log("B is Big");
+// }
+// else{
+//     console.log("C is Big");
+// }
+
+
+
+/* 8 = Electricity */
+
+// let total ;
+// let unit = Number(prompt("Enter Unit : "));
+
+// console.log("Unit Is : "+unit);
+
+// if (unit >= 0 && unit <= 100) {
+//     total = unit * 0.9 + 50 ;
+// }
+// else if (unit > 100 && unit <= 300) {
+//     total = 60 + (unit - 100) * 0.8 + 50 ;
+// }
+// else if (unit > 300) {
+//     total = 160 + (unit - 300) * 0.9 + 50 ;
+// }
+
+// if (unit > 300) {
+//     total = total + total * 0.15;
+//     console.log("Total is : ",total);
+// }
+// else{
+//     console.log("Total Is : ",total);
+// }
+
+/* 9 = Voting */
+
+// let age = Number(prompt("Enter Age : "));
+// console.log("Age Is : "+age);
+
+// if(age >= 18){
+//     console.log("Eligible For Vote");
+// }
+// else{
+//     console.log("Not Eligible For Vote");
+// }
+
+
+/* 10 = Salary Slab */
+
+// let basic = Number(prompt("Enter Basic : "));
+// console.log("Basic Is : ",+basic);
+
+// let gross;
+// let hra;
+// let da;
+
+// if (basic <= 5000) {
+//     hra = basic * 0.08;
+//     da = basic * 0.20;
+// }
+// else if (basic <= 10000) {
+//     hra = basic * 0.12;
+//     da = basic * 0.30;
+// }
+// else if (basic <= 15000) {
+//     hra = basic * 0.15;
+//     da = basic * 0.40;
+// }
+// else {
+//     hra = basic * 0.20;
+//     da = basic * 0.50;
+// }
+
+// gross = basic + hra + da;
+
+// console.log("HRA : ",hra);
+// console.log("DA : ",da);
+// console.log("Gross : ",gross);
+
+
+/* 11 = Income Tax Slab */
+
+// let income = Number(prompt("Enter Income : "));
+// console.log("Income Is : ",+income);
+
+// let tax;
+
+// if (income >= 0 && income <= 2500) {
+//     tax = 0.0;
+// }
+// else if(income >= 2500 && income <= 5000){
+//     tax = 0 + (income - 2500) * 0.10;
+// }
+// else if(income >= 5000 && income <= 10000){
+//     tax = 250 + (income - 5000) * 0.20;
+// }
+// else if(income >= 10000){
+//     tax = 1250 + (income - 10000) * 0.30;
+// }
+
+// console.log("Total Tax Is : ",tax);
+
+
+/* 12 = Students Marks */
+
+// let a = Number(prompt("Enter Maths : "));
+// let b = Number(prompt("Enter Science : "));
+// let c = Number(prompt("Enter English : "));
+
+// console.log("Maths Marks : "+a);
+// console.log("Science Marks : "+b);
+// console.log("English Marks : "+c);
+
+// let marks = a + b + c;
+// let per = marks / 3;
+
+// console.log("Obtain Marks :"+marks);
+// console.log("Perchantage : "+per);
+
+// if (marks > 75) {
+//     console.log("Grad A");
+// }
+// else if(marks<=75 && marks>=60){
+//     console.log("Grad B");
+// }
+// else if(marks<=60 && marks>=45){
+//     console.log("Grad C");
+// }
+// else if(marks<=45 && marks>=35){
+//     console.log("Grad D");
+// }
+// else
+// {
+//     console.log("Fail");
+// }
+
+
+/* 13 = Assign Value */
+
+// let x = Number(prompt("Enter X : "));
+// let y = Number(prompt("Enter Y : "));
+
+// if (x < 2000 || x > 3000) {
+//     console.log("Value Of X is : "+x);
+// }
+// if (100 < y && y < 500) {
+//     console.log("Value Of Y is : "+y);
+// }
