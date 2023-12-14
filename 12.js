30706794036c51e683b933030be4fea7c91a4a08
Dhaval Solanki <dhavalas24@gmail.com>
2023-12-14 15:58:59 +0530

    14 Dec

--- 12.js
+++ 12.js
@@ -1,4 +1,4 @@
-let data = [11,22,33,44,55,66,77,88];
+// let data = [11,22,33,44,55,66,77,88];
 
 /*======| Max & Min |========*/
 // MaxNum = (a) =>{
@@ -45,3 +45,70 @@ let data = [11,22,33,44,55,66,77,88];
 //     return a + val;
 // })
 // console.log(abc);
+
+/*======| Some |========*/
+// let abc;
+// abc = data.some((val, index)=>{
+//     return val < 25;
+// })
+// console.log(abc);
+        
+/*======| Every |========*/
+// let abc;
+// abc = data.every((val, index)=>{
+//     return val >= 25;
+// })
+// console.log(abc);
+
+/*======| Find |========*/
+// let abc;
+// abc = data.find((val, index)=>{
+//     return val <= 25;
+// })
+// console.log(abc);
+
+/*======| FindIndex |========*/
+// let abc;
+// abc = data.findIndex((val, index)=>{
+//     return val >= 25;
+// })
+// console.log(abc);
+
+/*======| Entries |========*/
+// let abc;
+// abc = data.entries();
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+// console.log(abc.next().value);
+
+
+/*======| Changing Element |========*/
+
+// data[5] = 99;
+// console.log(data);
+
+
+/*======| Concat (Merging Array) |========*/
+// const data = ["M","S"];
+// const a = ["Dhoni"];
+// const b = data.concat(a);
+
+// console.log(b);
+
+/*======| Include |========*/
+
+// let data = [11,22,33,44,55,66,77,88];
+// abc = data.includes(11);
+// console.log(abc);
+
+/*======| CopyWithin |========*/
+
+// let data = [11,22,33,44,55,66,77,88];
+// console.log(data.copyWithin(0,4,7));
+// console.log(data.copyWithin(0,7));
+// console.log(data.copyWithin(3,4,6));
