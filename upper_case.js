ccdc630876c63dddb2c2d74f0f37686e5c3d92b3
Dhaval Solanki <dhavalas24@gmail.com>
2023-12-16 09:48:06 +0530

    16 Dec

--- upper_case.js
+++ upper_case.js
@@ -1,15 +1,3 @@
-// let alph = prompt("Enter Alphabetic String : ");
-
-// let r = upperCase(alph);
-// console.log("First Letter Upper Case : ",r);
-
-// function upperCase(string) {
-//     let wordS = string.split('');
-//     let wordSec = charAt(0).toUppercase();
-//     let words = string.slice(1);
-//     return wordS + wordSec + words;
-// }
-// .join('');
 let alph = prompt("Enter Alphabetic String: ");
 
 let r = upperCase(alph);
@@ -27,32 +15,3 @@ function upperCase(string) {
 
     return result.join(' ');
 }
-
-    
-
-
-
-
-
-// function convertFirstLetterToUppercase(str) {
-//     // Split the string into an array of words
-//     const words = str.split(' ');
-  
-//     // Iterate over each word and convert the first letter to uppercase
-//     const convertedWords = words.map(word => {
-//       const firstLetter = word.charAt(0).toUpperCase();
-//       const restOfWord = word.slice(1);
-//       return firstLetter + restOfWord;
-//     });
-  
-//     // Join the modified words back into a string
-//     const convertedString = convertedWords.join(' ');
-  
-//     return convertedString;
-//   }
-  
-//   // Example usage
-//   const inputString = 'the quick brown fox';
-//   const convertedString = convertFirstLetterToUppercase(inputString);
-//   console.log(convertedString); // Output: 'The Quick Brown Fox'
-  
