257cdac1156402ae867be3c20c4881c78c5924cc
Dhaval Solanki <dhavalas24@gmail.com>
2023-12-21 15:52:02 +0530

    21 Dec

--- 17.js
+++ 17.js
@@ -1,7 +1,7 @@
 // JavaScript 'use strict'
 // In JavaScript, 'use strict'; status that the code should be executed in 'strict mode'. This makes it easier to write good and secure to write good and secure JS code.
 
-'use strict';
+// 'use strict';
 
 // {
 //     myVariable = 9;
@@ -58,10 +58,103 @@
 
 
 // Things Not Allowed in Strict Mode.
-// 1. Underclared variable is not allowed.
+// 1. Undeclared variable is not allowed.
 // {
 //     'use strict';
 
     // a = 'hello';  // throws an error
 // }
 
+// 2. Undeclared objects are not allowed.
+
+// {
+//     'use strict';
+
+//     person = {name: 'Carla', age:25};  // throws an error
+// }
+
+// 3. Deleting an objects is not allowed.
+
+// {
+//     'use strict';
+
+//     let person = {name: 'carla'};
+
+//     delete person;    // throws an error
+// }
+
+// 4. Duplicating a parameter name is not allowed
+
+// {
+//     'use strict';
+
+//     function hello(p1, p1){console.log('hello');}   //throws an error
+
+//     hello();
+// }
+
+// 5. Assigning to a noo-writable property is not allowed.
+
+// {
+//     'use strict';
+
+//     let obj1 = {};
+
+//     Object.defineProperty(obj1, 'x', {value : 7, writable: false});
+
+//     // assignment to a non-writable property
+//     obj1.x = 9;  // throw an error
+// }
+
+// 6. Assigning to a getter-only property is not allowed.
+
+// {
+//     'use strict';
+
+//     let obj2 = { get x() {return 7; } };
+
+//     // assignment to a getter-only property.
+//     obj2.x = 77; // throws an error
+// }
+
+// 7. Assigning to a new property on a non-extensible object is not allowed.
+
+// {
+//     'use strict';
+
+//     let obj = {};
+//     Object.preventExtensions(obj);
+//     // Assignment to a new property on a non-extensible object.
+//     obj.newValue = 'new value';  // throws an error
+// }
+
+// 8. Octal syntax is not allowed.
+
+// {
+//     'use strict';
+
+//     let a = 010;  // throws an error
+// }
+
+// 9. The variable name arguments and evel are not allowed.
+
+// {
+//     'use strict';
+
+//     let arguments = 'hello';  // throes an error
+
+//     let evel = 77;
+// }
+
+// 10. You cannot also use these reserved keywords in strict mode. 
+
+// implements interface let package private protected public static yield.
+
+
+// Benefits of Strict Mode.
+
+// The use of strict mode:
+
+// helps to write a cleaner code
+// changes previously accepted silent errors (bad syntax) into real errors and throws an error Message.
+// makes it easier to write "Secure" JavaScript
