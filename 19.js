c607884ecdfd8191fb2a27d7ee3fd7c2049c2d48
Dhaval Solanki <dhavalas24@gmail.com>
2023-12-22 09:56:43 +0530

    22 Dec

---
+++ 19.js
@@ -0,0 +1,29 @@
+/* JavaScript HTML DOM Events */
+
+// function message(){
+//     alert("Yes,I Am Heading Tag");
+// }
+
+// function datashow(){
+//     document.getElementById("date").innerHTML=Date();
+// }
+
+// function upperCase(){
+//     const change = document.getElementById("emails");
+//     change.value = change.value.toUpperCase();
+// }
+
+function red(one) {
+    one.style.color = "red";
+}
+// {
+//     function Down(obj){
+//         obj.style.backgroundColor = "#1ec5e5";
+//         obj.innerHTML = "Release Me";
+//     }
+
+//     function Up(obj){
+//         obj.style.backgroundColor = "#D94A38";
+//         obj.innerHTML = "Thank You";
+//     }
+// }
