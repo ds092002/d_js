2f80845fc50e14cde14868589c3c44b843d22cfc
Dhaval Solanki <dhavalas24@gmail.com>
2023-12-24 15:44:46 +0530

    24 Dec

---
+++ for_practis.js
@@ -0,0 +1,27 @@
+// JavaScript is a powerful programming language that can add interactivity to a website
+// It was invented by Brendan Eich in 1997.
+
+// output method :
+
+// 1). console.log();
+// 2). alert(" ");
+// 3). document.write(" ");
+
+// Q).JavaScript contains a which type of library ?
+// => A).JavaScript contains a standard library of objects, like Array, Date, and Math, and a core set of language elements like operators, control structures, and statements.
+
+
+// Ways to Declare a JavaScript Variable:
+// 3 type
+// ● Using var x = 5;
+// ● Using let y = 10;
+// ● Using const d = 7;
+// ● Using nothing
+
+var x = 10;
+var y = "World"
+console.log("X is : "+x);
+console.log("Hello "+y);
+// alert("Hello One!!!!");
+// document.write("Lerning JS",x);
+
